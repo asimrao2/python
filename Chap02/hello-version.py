c17a7a5064ae9bf8ac4b1bc49a061db078c8547f
asimrao2 <44842974+asimrao2@users.noreply.github.com>
2018-11-07 10:35:57 -0500

    Add files via upload

---
+++ Chap02/hello-version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+import platform
+
+print('This is python version {}'.format(platform.python_version()))
+print('This is machine {}'.format(platform.machine()))
+print('This is node {}'.format(platform.node()))
+print('This is processor {}'.format(platform.processor()))
+print('This is version {}'.format(platform.version()))
+
+
